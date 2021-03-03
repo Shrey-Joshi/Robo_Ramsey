717434a81314f4ea3c9f9d0d499f9438d6852d74
Dpmon1 <shreyjoshi3103@gmail.com>
2021-03-03 10:55:58 -0500

    Deleted shrey-joshi.js

--- shrey-joshi.js
+++
@@ -1,4 +0,0 @@
-while(True){
-	alert("Hello. I am Shrey Joshi.")
-	console.log("Go comprehend this haha: https://cdn.discordapp.com/attachments/403381854012571663/816122536148467732/5ik1hby4ngk61.png ")
-}
